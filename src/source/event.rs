d363a7a7bb9d61d4e1cfb9b324cc004374f305e8
Maccraft123 <maccraft123mc@gmail.com>
2022-10-08 22:40:44 +0200

    Try our best to get fancy device name of usb device

--- src/source/event.rs
+++ src/source/event.rs
@@ -11,27 +11,88 @@ use std::{
     fs,
 };
 
+fn usb_manufacturer_product(input: String) -> Option<String> {
+    // input: usb-0000:09:00.3-3/input0
+    if let Some(path_with_input) = input.strip_prefix("usb-") {
+        // 0000:09:00.3-3/input0
+        let path_itself = path_with_input.split('/')
+            .map(|v| v.to_string())
+            .collect::<Vec<String>>()
+            .swap_remove(0);
+        
+        // path_vec = vec!["0000:09:00.3", 3]
+        let path_vec = path_itself.split('-')
+            .map(|v| v.to_string())
+            .collect::<Vec<String>>();
+
+        if let Some((pci_id, usb_path)) = path_vec.get(0).zip(path_vec.get(1)) {
+            let path: PathBuf = ["/sys/bus/pci/devices/", pci_id].iter().collect();
+            if !path.exists() {
+                return None;
+            }
+
+            let usb_bus: u32 = usb_path.parse().unwrap();
+            let final_path: PathBuf = [
+                "/sys/bus/pci/devices/",
+                pci_id,
+                &format!("usb{}", usb_bus),
+                &format!("{}-{}", usb_bus, usb_path)
+            ].iter().collect();
+
+            if !final_path.exists() {
+                return None;
+            }
+
+            let manufacturer_raw = fs::read_to_string(final_path.join("manufacturer")).unwrap_or_default();
+            let product_raw = fs::read_to_string(final_path.join("product")).unwrap_or_default();
+
+
+            let manufacturer = manufacturer_raw.trim();
+            let product = product_raw.trim();
+
+            if product.starts_with(&manufacturer) {
+                return Some(product.to_string());
+            }
+
+            if manufacturer.is_empty() || product.is_empty() {
+                return Some(format!("{}{}", manufacturer, product));
+            }
+
+            return Some(format!("{} {}", manufacturer, product));
+        }
+    }
+
+    None
+}
+
 pub struct Evdev {
-    my_device: Device,
-    my_path: PathBuf,
+    device: Device,
+    path: PathBuf,
+    name: Option<String>,
 }
 
 unsafe impl Send for Evdev{}
 unsafe impl Sync for Evdev{}
 
 impl Evdev {
-    fn new(path: PathBuf, mut my_device: Device) -> Option<Self> {
+    fn new(path: PathBuf, mut device: Device) -> Option<Self> {
         // check for gamepads
-        if !my_device.supported_keys().map_or(false, |k| k.contains(Key::BTN_SOUTH)) {
+        if !device.supported_keys().map_or(false, |k| k.contains(Key::BTN_SOUTH)) {
             return None;
         }
 
-        my_device.grab().ok()?;
-        fs::remove_file(&path).ok()?;
+        device.grab().ok()?;
+        //fs::remove_file(&path).ok()?;
+
+        let mut name = None;
+        if let Some(phys_path) = device.physical_path() {
+            name = usb_manufacturer_product(phys_path.to_string());
+        }
 
         Some(Self {
-            my_device,
-            my_path: path,
+            device,
+            path,
+            name,
         })
     }
 }
@@ -56,10 +117,16 @@ impl EventSource for Evdev {
         Ok(())
     }
     fn name(self: &Evdev) -> String {
-        self.my_device.name().unwrap_or("Linux event device").to_string()
+        if let Some(n) = self.name.clone() {
+            return n;
+        }
+        if let Some(n) = self.device.name() {
+            return n.to_string();
+        }
+        "Linux event device".to_string()
     }
     fn path(self: &Evdev) -> String {
-        self.my_device.physical_path().unwrap_or("Unknown").to_string()
+        self.device.physical_path().unwrap_or("Unknown").to_string()
     }
     fn close(self: Evdev) {
 
